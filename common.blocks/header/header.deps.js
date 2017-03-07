7a3d4d1422798720ed7f8e097fd5350b82adff32
alios <abc.ua@yandex.ru>
2017-03-07 17:04:22 +0200

    up

--- common.blocks/header/header.deps.js
+++
@@ -1,7 +0,0 @@
-({
-    shouldDeps : [
-        { block : 'button', mods : { theme : 'sir' } },
-        { block : 'icon', mods : { button : ['profile-orange', 'search-orange'] } },
-        { block : 'popup', mods : { autoclosable : true, target : 'anchor', theme : 'islands' } }
-    ]
-})
