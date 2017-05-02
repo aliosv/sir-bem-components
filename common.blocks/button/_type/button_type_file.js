5e4e11f143927601f95f0b11638f57b38b830dd4
alios <abc.ua@yandex.ru>
2017-05-02 04:32:09 +0300

    up

--- common.blocks/button/_type/button_type_file.js
+++ common.blocks/button/_type/button_type_file.js
@@ -6,7 +6,16 @@ modules.define('button', [], function(provide, Block) {
                 inited : function() {
                     this.__base.apply(this, arguments);
 
+                    var _this = this;
+
                     this._input = this.domElem.children('input');
+                    this._input.on('change', function() {
+                        _this._emit('change');
+                    });
+
+                    this._events().on('click', function() {
+                        _this._input.click();
+                    });
                 }
             }
         },
@@ -14,17 +23,5 @@ modules.define('button', [], function(provide, Block) {
         getVal : function() {
             return this._input.get(0).files;
         }
-    }, /** @lends button */{
-        onInit : function() {
-            this.__base.apply(this, arguments);
-
-            this._domEvents().on('change', function() {
-                this._emit('change');
-            });
-
-            this._events().on('click', function(e) {
-                this._input.click();
-            });
-        }
-    }));
+    }, /** @lends button */{}));
 });
